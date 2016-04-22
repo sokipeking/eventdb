f71cf9a787661d35de574a945fc8333559196aa7
Joy Du <dooyoe@gmail.com>
2016-04-22 19:42:12 +0800

    change create account page

--- includes/common.php
+++ includes/common.php
@@ -3,9 +3,9 @@ session_start();
 define("TEMPLATE_DIR", "templates/template_content_angularjs/");
 
 define ("DB_NAME", "eventdb");
-define ("DB_HOST", "127.0.0.1");
+define ("DB_HOST", "127.1");
 define ("DB_PORT", "5432");
-define ("DB_USER", "soki");
+define ("DB_USER", "duyao");
 define ("DB_PASSWORD", "");
 
 global $dbh;
