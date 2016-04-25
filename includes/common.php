6ddd83e4a526519c108df884c18f8b7748dce097
Joy Du <dooyoe@gmail.com>
2016-04-25 15:40:58 +0800

    clear common setting

--- includes/common.php
+++ includes/common.php
@@ -2,12 +2,12 @@
 session_start();
 define("TEMPLATE_DIR", "templates/template_content_angularjs/");
 
-define ("DB_NAME", "eventdb");
-define ("DB_HOST", "127.1");
-define ("DB_PORT", "5432");
-define ("DB_USER", "duyao");
+define ("DB_NAME", "");
+define ("DB_HOST", "");
+define ("DB_PORT", "");
+define ("DB_USER", "");
 define ("DB_PASSWORD", "");
 
 global $dbh;
-$dbh = new PDO("pgsql:dbname=".DB_NAME.";host=".DB_HOST, DB_USER, DB_PASSWORD); 
+$dbh = new PDO("pgsql:dbname=".DB_NAME.";host=".DB_HOST, DB_USER, DB_PASSWORD);
 ?>
